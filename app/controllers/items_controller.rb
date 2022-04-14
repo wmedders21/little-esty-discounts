2f96c833c43739b9154b0f20dd0de9e82c509be0
James Harkins <jh.harkins@protonmail.com>
2022-04-14 11:10:38 -0600

    Refactor: update merchant items and merchant invoices routes to use resources syntax

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,6 @@
 class ItemsController < ApplicationController
   def index
-    @merchant = Merchant.find(params[:id])
+    @merchant = Merchant.find(params[:merchant_id])
   end
 
   def show
@@ -8,11 +8,11 @@ class ItemsController < ApplicationController
   end
 
   def new
-    @merchant = Merchant.find(params[:id])
+    @merchant = Merchant.find(params[:merchant_id])
   end
 
   def create
-    merchant = Merchant.find(params[:id])
+    merchant = Merchant.find(params[:merchant_id])
     item = merchant.items.create(item_params)
     redirect_to "/merchants/#{merchant.id}/items"
   end
