815852418995c4baa6a3b62676aa716d025509b9
Will Medders <wmedders@gmail.com>
2022-04-14 19:55:12 -0400

    Feat: admin invoices show has a drop down to select invoice status

--- app/controllers/admin/invoices_controller.rb
+++ app/controllers/admin/invoices_controller.rb
@@ -6,4 +6,10 @@ class Admin::InvoicesController < ApplicationController
   def show
     @invoice = Invoice.find(params[:id])
   end
+
+  def update
+    invoice = Invoice.find(params[:id])
+    invoice.update(status: params[:status])
+    redirect_to "/admin/invoices/#{invoice.id}"
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,23 +1,16 @@
 Rails.application.routes.draw do
 
-  get '/merchants/:id/dashboard', to: 'merchants#show'
-
-  namespace :merchants do
-    get '/:id/items', to: 'items#index'
-    get '/:id/items/new', to: 'items#new'
-    post '/:id/items', to: 'items#create'
-    get '/:id/items/:id', to: 'items#show'
-    get '/:id/items/:id/edit', to: 'items#edit'
-    patch '/:id/items/:id', to: 'items#update'
-    get '/:id/invoices', to: 'invoices#index'
-    get '/:id/invoices/:id', to: 'invoices#show'
+  resources :merchants do
+    resources :items
+    resources :invoices
   end
+
+  get '/merchants/:id/dashboard', to: 'merchants#show'
 
   resources :admin, only: [:index]
 
-
   namespace :admin do
-    resources :invoices, only: [:index, :show]
+    resources :invoices, only: [:index, :show, :update]
     resources :merchants, only: [:index, :show]
   end
 end
