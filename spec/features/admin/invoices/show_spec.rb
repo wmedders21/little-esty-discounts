123cda3a2302e1eaa38722ee07390c517edfedff
Will Medders <wmedders@gmail.com>
2022-04-13 21:51:42 -0400

    FEATUREtest: admin invoices show displays total revenue

--- spec/features/admin/invoices/show_spec.rb
+++ spec/features/admin/invoices/show_spec.rb
@@ -27,7 +27,7 @@ RSpec.describe "Admin Invoices Show" do
     InvoiceItem.create!(invoice_id: invoice_1.id, item_id: item_2.id, quantity: 2, status: 0, unit_price: 215)
 
     visit "/admin/invoices/#{invoice_1.id}"
-    
+
     within("#item-#{item_1.id}") do
       expect(page).to have_content("pickle")
       expect(page).to have_content("6")
@@ -43,5 +43,21 @@ RSpec.describe "Admin Invoices Show" do
       expect(page).to have_content("pending")
       expect(page).to have_no_content("pickle")
     end
+  end
+
+  it 'displays the total revenue for the invoice' do
+    walmart = Merchant.create!(name: "Wal-Mart")
+    bob = Customer.create!(first_name: "Bob", last_name: "Benson")
+    item_1 = walmart.items.create!(name: "pickle", description: "sour cucumber", unit_price: 300)
+    item_2 = walmart.items.create!(name: "eraser", description: "rubber bit", unit_price: 200)
+
+    invoice_1 = bob.invoices.create!(status: 1, created_at: '05 Apr 2022 00:53:36 UTC +00:00')
+
+    InvoiceItem.create!(invoice_id: invoice_1.id, item_id: item_1.id, quantity: 6, status: 1, unit_price: 295)
+    InvoiceItem.create!(invoice_id: invoice_1.id, item_id: item_2.id, quantity: 2, status: 0, unit_price: 215)
+
+    visit "/admin/invoices/#{invoice_1.id}"
+
+    expect(page).to have_content("$22.00")  
   end
 end
