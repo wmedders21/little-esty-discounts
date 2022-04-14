7a788a9c94397a3e0099c37371aedf91c33a5b38
Will Medders <wmedders@gmail.com>
2022-04-14 13:04:13 -0400

    Refactor FEATUREtest: admin invoices show has a select field for status that can update the status

--- spec/features/admin/invoices/show_spec.rb
+++ spec/features/admin/invoices/show_spec.rb
@@ -9,7 +9,7 @@ RSpec.describe "Admin Invoices Show" do
     visit "/admin/invoices/#{invoice_1.id}"
     # save_and_open_page
     expect(page).to have_content("#{invoice_1.id}")
-    expect(page).to have_content("completed")
+    expect(page).to have_content("Completed")
     expect(page).to have_content("Bob Benson")
     expect(page).to have_content('Friday, July 01, 2022')
     expect(page).to have_no_content('Saturday, April 02, 2022')
@@ -67,21 +67,23 @@ RSpec.describe "Admin Invoices Show" do
     item_1 = walmart.items.create!(name: "pickle", description: "sour cucumber", unit_price: 300)
     item_2 = walmart.items.create!(name: "eraser", description: "rubber bit", unit_price: 200)
 
-    invoice_1 = bob.invoices.create!(status: 1, created_at: '05 Apr 2022 00:53:36 UTC +00:00')
+    invoice_1 = bob.invoices.create!(status: 0, created_at: '05 Apr 2022 00:53:36 UTC +00:00')
 
     InvoiceItem.create!(invoice_id: invoice_1.id, item_id: item_1.id, quantity: 6, status: 1, unit_price: 295)
     InvoiceItem.create!(invoice_id: invoice_1.id, item_id: item_2.id, quantity: 2, status: 0, unit_price: 215)
 
     visit "/admin/invoices/#{invoice_1.id}"
-    
-    expect(page).to have_checked_field("in progress")
-    expect(page).to have_unchecked_field(:completed)
-    expect(page).to have_unchecked_field(:cancelled)
 
-    select :completed, :from => "status_select"
+    expect(page).to have_select(:status, selected: 'In Progress')
 
-    expect(page).to have_checked_field(:completed)
-    expect(page).to have_unchecked_field("in progress")
-    expect(page).to have_unchecked_field(:cancelled)
+    select 'Completed', :from => :status
+    click_button("Update Status")
+
+    expect(page).to have_select(:status, selected: 'Completed')
+
+    select 'Cancelled', :from => :status
+    click_button("Update Status")
+
+    expect(page).to have_select(:status, selected: 'Cancelled')
   end
 end
