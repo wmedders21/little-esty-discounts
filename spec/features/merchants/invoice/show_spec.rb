0ec2f4bfefc52193191d468724c74765485b83d8
Drew Proebstel <d.proebstel@gmail.com>
2022-04-14 20:12:27 -0700

    spec: add item price and quantity test

--- spec/features/merchants/invoice/show_spec.rb
+++ spec/features/merchants/invoice/show_spec.rb
@@ -9,11 +9,24 @@ RSpec.describe 'the merchant invoice show page' do
         invoice_1 = bob.invoices.create!(status: 1, created_at: '05 Apr 2022 00:53:36 UTC +00:00')
 
         visit "/merchants/#{merchant.id}/invoices/#{invoice_1.id}"
-        save_and_open_page
+
         expect(page).to have_content("#{invoice_1.id}")
         expect(page).to have_content("completed")
         expect(page).to have_content("Bob Benson")
         expect(page).to have_content('Tuesday, April 05, 2022')
 
+  end
+
+
+   it 'the quatity and price of item sold' do
+        merchant = Merchant.create(name: "Braum's")
+        item1 = merchant.items.create(name: "Toast", description: "Let it rip!", unit_price: 1000)
+        bob = Customer.create!(first_name: "Bob", last_name: "Benson")
+        invoice_1 = bob.invoices.create!(status: 1, created_at: '05 Apr 2022 00:53:36 UTC +00:00')
+        invoice_item_1 = item1.invoice_items.create(invoice_id:invoice_1.id, quantity:45, unit_price: 1000)
+        visit "/merchants/#{merchant.id}/invoices/#{invoice_1.id}"
+
+        expect(page).to have_content("45")
+        expect(page).to have_content("1000")
   end
 end
