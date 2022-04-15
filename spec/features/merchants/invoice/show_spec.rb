ddee77d7c629f6572db6cd1f9fc8c89d1970bdcc
Drew Proebstel <d.proebstel@gmail.com>
2022-04-14 20:41:36 -0700

    Spec:does not show info from other merchants

--- spec/features/merchants/invoice/show_spec.rb
+++ spec/features/merchants/invoice/show_spec.rb
@@ -18,15 +18,38 @@ RSpec.describe 'the merchant invoice show page' do
   end
 
 
-   it 'the quatity and price of item sold' do
+   it 'shows the quatity and price of item sold' do
         merchant = Merchant.create(name: "Braum's")
         item1 = merchant.items.create(name: "Toast", description: "Let it rip!", unit_price: 1000)
         bob = Customer.create!(first_name: "Bob", last_name: "Benson")
         invoice_1 = bob.invoices.create!(status: 1, created_at: '05 Apr 2022 00:53:36 UTC +00:00')
         invoice_item_1 = item1.invoice_items.create(invoice_id:invoice_1.id, quantity:45, unit_price: 1000)
         visit "/merchants/#{merchant.id}/invoices/#{invoice_1.id}"
-        save_and_open_page
+
         expect(page).to have_content("45")
         expect(page).to have_content("1000")
+  end
+
+   it 'the quatity and price of item sold' do
+        merchant = Merchant.create(name: "Braum's")
+        merchant2 = Merchant.create(name: "Target")
+
+        item1 = merchant.items.create(name: "Toast", description: "Let it rip!", unit_price: 1000)
+        item2 = merchant.items.create(name: "Polearm", description: "Let it rip!", unit_price: 1000)
+
+        bob = Customer.create!(first_name: "Bob", last_name: "Benson")
+        dave = Customer.create!(first_name: "Dave", last_name: "Fogherty")
+
+        invoice_1 = bob.invoices.create!(status: 1, created_at: '05 Apr 2022 00:53:36 UTC +00:00')
+        invoice_2 = dave.invoices.create!(status: 1, created_at: '05 Apr 2022 00:53:36 UTC +00:00')
+
+        invoice_item_1 = item1.invoice_items.create(invoice_id:invoice_1.id, quantity:45, unit_price: 1000)
+        invoice_item_2 = item2.invoice_items.create(invoice_id:invoice_1.id, quantity:222, unit_price: 3499)
+        visit "/merchants/#{merchant.id}/invoices/#{invoice_1.id}"
+
+        expect(page).to_not have_content("222")
+        expect(page).to_not have_content("3499")
+        expect(page).to_not have_content("Dave")
+        expect(page).to_not have_content("Fogherty")
   end
 end
