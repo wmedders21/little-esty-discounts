be48ce87b1c0c99cc34a15d2d19c75156dbfd6a5
Luke Pascale <lpluke5@gmail.com>
2022-04-16 18:19:10 -0500

    FeatTest: Admin merch create page spec file

---
+++ spec/features/admin/merchant/create_spec.rb
@@ -0,0 +1,31 @@
+require 'rails_helper'
+
+RSpec.describe 'the admin merchant create page' do
+  it 'has a link to create new merchant on index page' do
+    merchant_1 = Merchant.create!(name: "Mollys", status: 1)
+
+    visit admin_merchants_path
+
+    expect(page).to have_link("Create New Merchant")
+  end
+
+  it 'has a form which can add merchant info' do
+    merchant_1 = Merchant.create!(name: "Mollys", status: 1)
+
+    visit admin_merchants_path
+
+    click_link "Create New Merchant"
+    fill_in :name, with: "Bobby Sues"
+
+    click 'Submit'
+
+    expect(current_path).to eq(admin_merchants_path)
+    expect(page).to have_content("Bobby Sues")
+  end
+
+  it 'the default status for a new merchant is disabled' do
+    merchant_1 = Merchant.create!(name: "Mollys")
+
+    expect(merchant_1.status).to eq('disabled')
+  end
+end
