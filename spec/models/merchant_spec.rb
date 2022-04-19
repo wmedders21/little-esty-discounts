2852f6c3fa8b692a7e009cad28a91a629ffd7618
Drew Proebstel <d.proebstel@gmail.com>
2022-04-19 15:11:03 -0700

    add model test

--- spec/models/merchant_spec.rb
+++ spec/models/merchant_spec.rb
@@ -104,4 +104,75 @@ RSpec.describe Merchant, type: :model do
       end
     end
   end
+  describe 'class methods' do
+    describe '.top_five_by_revenue' do
+      it "displays the top five merchants name and revenue" do
+          merchant1 = Merchant.create(name: "Merchant 1")
+          merchant2 = Merchant.create(name: "Merchant 2")
+          merchant3 = Merchant.create(name: "Merchant 3")
+          merchant4 = Merchant.create(name: "Merchant 4")
+          merchant5 = Merchant.create(name: "Merchant 5")
+          merchant6 = Merchant.create(name: "Merchant 6")
+          merchant7 = Merchant.create(name: "Merchant 7")
+
+          item1 = merchant1.items.create(name: "Apple", description: "Let it rip!", unit_price: 1500)
+          item2 = merchant1.items.create(name: "Banana", description: "Let it rip!", unit_price: 2000)
+          item3 = merchant2.items.create(name: "Coconut", description: "Let it rip!", unit_price: 700)
+          item4 = merchant3.items.create(name: "Date", description: "Let it rip!", unit_price: 123)
+          item5 = merchant4.items.create(name: "Eggplant", description: "Let it rip!", unit_price: 500)
+          item6 = merchant5.items.create(name: "Fennel", description: "Let it rip!", unit_price: 60)
+          item7 = merchant6.items.create(name: "Grape", description: "Let it rip!", unit_price: 10)
+          item8 = merchant7.items.create(name: "Habenero", description: "Let it rip!", unit_price: 325)
+
+          bob = Customer.create!(first_name: "Bob", last_name: "Benson")
+          dave = Customer.create!(first_name: "Dave", last_name: "Fogherty")
+
+          invoice_1 = bob.invoices.create!(status: 1, created_at: '05 Apr 2022 00:53:36 UTC +00:00')
+          invoice_2 = bob.invoices.create!(status: 1, created_at: '05 Apr 2022 00:53:36 UTC +00:00')
+          invoice_3 = bob.invoices.create!(status: 1, created_at: '05 Apr 2022 00:53:36 UTC +00:00')
+          invoice_4 = bob.invoices.create!(status: 1, created_at: '05 Apr 2022 00:53:36 UTC +00:00')
+          invoice_5 = bob.invoices.create!(status: 1, created_at: '05 Apr 2022 00:53:36 UTC +00:00')
+          invoice_6 = bob.invoices.create!(status: 1, created_at: '05 Apr 2022 00:53:36 UTC +00:00')
+          invoice_7 = bob.invoices.create!(status: 1, created_at: '05 Apr 2022 00:53:36 UTC +00:00')
+          invoice_8 = dave.invoices.create!(status: 1, created_at: '05 Apr 2022 00:53:36 UTC +00:00')
+          invoice_9 = dave.invoices.create!(status: 1, created_at: '05 Apr 2022 00:53:36 UTC +00:00')
+          invoice_10 = dave.invoices.create!(status: 1, created_at: '05 Apr 2022 00:53:36 UTC +00:00')
+          invoice_11 = dave.invoices.create!(status: 1, created_at: '05 Apr 2022 00:53:36 UTC +00:00')
+          invoice_12 = dave.invoices.create!(status: 1, created_at: '05 Apr 2022 00:53:36 UTC +00:00')
+
+          invoice_item_1 = item1.invoice_items.create(invoice_id:invoice_1.id, quantity:21, unit_price: 1500)
+          invoice_item_2 = item2.invoice_items.create(invoice_id:invoice_1.id, quantity:10, unit_price: 2000)
+          invoice_item_3 = item3.invoice_items.create(invoice_id:invoice_1.id, quantity:13, unit_price: 700)
+          invoice_item_4 = item4.invoice_items.create(invoice_id:invoice_1.id, quantity:2, unit_price: 123)
+          invoice_item_5 = item5.invoice_items.create(invoice_id:invoice_1.id, quantity:9, unit_price: 500)
+          invoice_item_6 = item6.invoice_items.create(invoice_id:invoice_1.id, quantity:19, unit_price: 60)
+          invoice_item_7 = item1.invoice_items.create(invoice_id:invoice_1.id, quantity:12, unit_price: 10)
+          invoice_item_8 = item1.invoice_items.create(invoice_id:invoice_1.id, quantity:11, unit_price: 1500)
+          invoice_item_9 = item1.invoice_items.create(invoice_id:invoice_1.id, quantity:10, unit_price: 1500)
+          invoice_item_10 = item2.invoice_items.create(invoice_id:invoice_1.id, quantity:8, unit_price: 2000)
+          invoice_item_11 = item2.invoice_items.create(invoice_id:invoice_1.id, quantity:31, unit_price: 2000)
+          invoice_item_12 = item3.invoice_items.create(invoice_id:invoice_1.id, quantity:1, unit_price: 700)
+
+          transactions_1 = invoice_1.transactions.create(credit_card_number: "*", credit_card_expiration_date: "*", result:"success" )
+          transactions_2 = invoice_2.transactions.create(credit_card_number: "*", credit_card_expiration_date: "*", result:"success" )
+          transactions_3 = invoice_3.transactions.create(credit_card_number: "*", credit_card_expiration_date: "*", result:"success" )
+          transactions_4 = invoice_4.transactions.create(credit_card_number: "*", credit_card_expiration_date: "*", result:"success" )
+          transactions_5 = invoice_5.transactions.create(credit_card_number: "*", credit_card_expiration_date: "*", result:"success" )
+          transactions_6 = invoice_6.transactions.create(credit_card_number: "*", credit_card_expiration_date: "*", result:"success" )
+          transactions_7 = invoice_7.transactions.create(credit_card_number: "*", credit_card_expiration_date: "*", result:"success" )
+          transactions_8 = invoice_8.transactions.create(credit_card_number: "*", credit_card_expiration_date: "*", result:"success" )
+          transactions_9 = invoice_9.transactions.create(credit_card_number: "*", credit_card_expiration_date: "*", result:"success" )
+          transactions_10 = invoice_10.transactions.create(credit_card_number: "*", credit_card_expiration_date: "*", result:"success" )
+          transactions_11 = invoice_11.transactions.create(credit_card_number: "*", credit_card_expiration_date: "*", result:"success" )
+          transactions_12 = invoice_12.transactions.create(credit_card_number: "*", credit_card_expiration_date: "*", result:"success" )
+
+          merchant = Merchant.top_five_by_revenue
+          expect(merchant[0].name).to eq('Merchant 1')
+          expect(merchant[1].name).to eq('Merchant 2')
+          expect(merchant[2].name).to eq('Merchant 4')
+          expect(merchant[3].name).to eq('Merchant 5')
+          expect(merchant[4].name).to eq('Merchant 3')
+      end
+    end
+  end
 end
