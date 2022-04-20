b1151eacd7883edc54d60454d40bf7db22ab6b0a
James Harkins <jh.harkins@protonmail.com>
2022-04-20 14:09:52 -0600

    Test: create tests for merges attribute for Repo instance

--- spec/poros/repo_spec.rb
+++ spec/poros/repo_spec.rb
@@ -6,11 +6,13 @@ RSpec.describe Repo do
                  contributers: [{name: "wmedders21", commits: 78},
                                 {name: "James-Harkins", commits: 55},
                                 {name: "enalihai", commits: 51},
-                                {name: "DrewProebstel", commits: 49}]}
+                                {name: "DrewProebstel", commits: 49}],
+                  merges: 90}
     repo = Repo.new(repo_data)
     expect(repo.class).to eq(Repo)
     expect(repo.name).to eq('little-esty-shop')
     expect(repo.contributers).to be_a(Array)
     expect(repo.contributers[0]).to be_a(Hash)
+    expect(repo.merges).to eq(90)
   end
 end
