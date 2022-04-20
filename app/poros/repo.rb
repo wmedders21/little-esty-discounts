4a747419c80999e1aa3a6fcd635b4c61237691ae
James Harkins <jh.harkins@protonmail.com>
2022-04-20 14:10:22 -0600

    Feat: pass tests for merge attribute for Repo instance

--- app/poros/repo.rb
+++ app/poros/repo.rb
@@ -1,8 +1,9 @@
 class Repo
-  attr_reader :name, :contributers
+  attr_reader :name, :contributers, :merges
 
   def initialize(data)
     @name = data[:name]
     @contributers = data[:contributers]
+    @merges = data[:merges]
   end
 end
