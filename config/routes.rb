0347d9e6551e7c69e3b0375927fe190608c0258a
James Harkins <jh.harkins@protonmail.com>
2022-04-20 14:52:24 -0600

    Refactor: update invoices and invoice_items_controller, invoices and invoice_items views, and invoices and invoice_items spec folder to include merchant_ prefix for better encapsulation

--- config/routes.rb
+++ config/routes.rb
@@ -8,9 +8,22 @@ Rails.application.routes.draw do
     get '/items/:item_id', to: 'merchant_items#show'
     patch '/items/:item_id', to: 'merchant_items#update'
     delete '/items/:item_id', to: 'merchant_items#destroy'
-    # resources :items
-    resources :invoices
-    resources :invoice_items
+
+    get '/invoices', to: 'merchant_invoices#index'
+    post '/invoices', to: 'merchant_invoices#create'
+    get '/invoices/new', to: 'merchant_invoices#new'
+    get '/invoices/:invoice_id/edit', to: 'merchant_invoices#edit'
+    get '/invoices/:invoice_id', to: 'merchant_invoices#show'
+    patch '/invoices/:invoice_id', to: 'merchant_invoices#update'
+    delete '/invoices/:invoice_id', to: 'merchant_invoices#destroy'
+
+    get '/invoice_items', to: 'merchant_invoice_items#index'
+    post '/invoice_items', to: 'merchant_invoice_items#create'
+    get '/invoice_items/new', to: 'merchant_invoice_items#new'
+    get '/invoice_items/:invoice_item_id/edit', to: 'merchant_invoice_items#edit'
+    get '/invoice_items/:invoice_item_id', to: 'merchant_invoice_items#show'
+    patch '/invoice_items/:invoice_item_id', to: 'merchant_invoice_items#update'
+    delete '/invoice_items/:invoice_item_id', to: 'merchant_invoice_items#destroy'
   end
 
   get '/merchants/:id/dashboard', to: 'merchants#show'
