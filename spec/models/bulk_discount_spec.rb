535d1ee5ddaa79c0c41e65d562d8c3793533a0e3
Will Medders <wmedders@gmail.com>
2022-04-24 11:54:33 -0400

    ModelTest: BulkDiscount validations

--- spec/models/bulk_discount_spec.rb
+++ spec/models/bulk_discount_spec.rb
@@ -4,4 +4,9 @@ RSpec.describe BulkDiscount do
   describe 'relationships' do
     it { should belong_to :merchant }
   end
+
+  describe 'validations' do
+    it { should validate_presence_of :discount_percentage}
+    it { should validate_presence_of :quantity_threshold}
+  end
 end
