014dac91b211a131ff8141e8bbfebb5f30963660
Will Medders <wmedders@gmail.com>
2022-04-24 19:29:37 -0400

    FeatureTest: merchant invoices show displays total discount revenue

--- spec/features/merchants/invoice/show_spec.rb
+++ spec/features/merchants/invoice/show_spec.rb
@@ -1,73 +1,92 @@
 require 'rails_helper'
 
 RSpec.describe 'the merchant invoice show page' do
-   it 'shows all the attributes for an invoice', :vcr do
-
-        merchant = Merchant.create(name: "Braum's")
-        item1 = merchant.items.create(name: "Toast", description: "Let it rip!", unit_price: 1000)
-        bob = Customer.create!(first_name: "Bob", last_name: "Benson")
-        invoice_1 = bob.invoices.create!(status: 1, created_at: '05 Apr 2022 00:53:36 UTC +00:00')
-        visit "/merchants/#{merchant.id}/invoices/#{invoice_1.id}"
-        expect(page).to have_content("#{invoice_1.id}")
-        expect(page).to have_content("completed")
-        expect(page).to have_content("Bob Benson")
-        expect(page).to have_content('Tuesday, April 05, 2022')
-
+  it 'shows all the attributes for an invoice', :vcr do
+    merchant = Merchant.create(name: "Braum's")
+    item1 = merchant.items.create(name: "Toast", description: "Let it rip!", unit_price: 1000)
+    bob = Customer.create!(first_name: "Bob", last_name: "Benson")
+    invoice_1 = bob.invoices.create!(status: 1, created_at: '05 Apr 2022 00:53:36 UTC +00:00')
+    visit "/merchants/#{merchant.id}/invoices/#{invoice_1.id}"
+    expect(page).to have_content("#{invoice_1.id}")
+    expect(page).to have_content("completed")
+    expect(page).to have_content("Bob Benson")
+    expect(page).to have_content('Tuesday, April 05, 2022')
   end
 
 
-   it 'shows the quatity and price of item sold', :vcr do
-        merchant = Merchant.create(name: "Braum's")
-        item1 = merchant.items.create(name: "Toast", description: "Let it rip!", unit_price: 1000)
-        bob = Customer.create!(first_name: "Bob", last_name: "Benson")
-        invoice_1 = bob.invoices.create!(status: 1, created_at: '05 Apr 2022 00:53:36 UTC +00:00')
-        invoice_item_1 = item1.invoice_items.create(invoice_id:invoice_1.id, quantity:45, unit_price: 1000)
-        visit "/merchants/#{merchant.id}/invoices/#{invoice_1.id}"
-
-        expect(page).to have_content("45")
-        expect(page).to have_content("10.00")
+  it 'shows the quatity and price of item sold', :vcr do
+    merchant = Merchant.create(name: "Braum's")
+    item1 = merchant.items.create(name: "Toast", description: "Let it rip!", unit_price: 1000)
+    bob = Customer.create!(first_name: "Bob", last_name: "Benson")
+    invoice_1 = bob.invoices.create!(status: 1, created_at: '05 Apr 2022 00:53:36 UTC +00:00')
+    invoice_item_1 = item1.invoice_items.create(invoice_id:invoice_1.id, quantity:45, unit_price: 1000)
+    visit "/merchants/#{merchant.id}/invoices/#{invoice_1.id}"
+    expect(page).to have_content("45")
+    expect(page).to have_content("10.00")
   end
 
-   it 'the quatity and price of item sold', :vcr do
-        merchant = Merchant.create(name: "Braum's")
-        merchant2 = Merchant.create(name: "Target")
+  it 'the quatity and price of item sold', :vcr do
+    merchant = Merchant.create(name: "Braum's")
+    merchant2 = Merchant.create(name: "Target")
 
-        item1 = merchant.items.create(name: "Toast", description: "Let it rip!", unit_price: 1000)
-        item2 = merchant2.items.create(name: "Polearm", description: "Let it rip!", unit_price: 1000)
+    item1 = merchant.items.create(name: "Toast", description: "Let it rip!", unit_price: 1000)
+    item2 = merchant2.items.create(name: "Polearm", description: "Let it rip!", unit_price: 1000)
 
-        bob = Customer.create!(first_name: "Bob", last_name: "Benson")
-        dave = Customer.create!(first_name: "Dave", last_name: "Fogherty")
+    bob = Customer.create!(first_name: "Bob", last_name: "Benson")
+    dave = Customer.create!(first_name: "Dave", last_name: "Fogherty")
+    invoice_1 = bob.invoices.create!(status: 1, created_at: '05 Apr 2022 00:53:36 UTC +00:00')
+    invoice_2 = dave.invoices.create!(status: 1, created_at: '05 Apr 2022 00:53:36 UTC +00:00')
 
-        invoice_1 = bob.invoices.create!(status: 1, created_at: '05 Apr 2022 00:53:36 UTC +00:00')
-        invoice_2 = dave.invoices.create!(status: 1, created_at: '05 Apr 2022 00:53:36 UTC +00:00')
+    invoice_item_1 = item1.invoice_items.create(invoice_id:invoice_1.id, quantity:45, unit_price: 1000)
+    invoice_item_2 = item2.invoice_items.create(invoice_id:invoice_1.id, quantity:222, unit_price: 1000)
+    visit "/merchants/#{merchant.id}/invoices/#{invoice_1.id}"
 
-        invoice_item_1 = item1.invoice_items.create(invoice_id:invoice_1.id, quantity:45, unit_price: 1000)
-        invoice_item_2 = item2.invoice_items.create(invoice_id:invoice_1.id, quantity:222, unit_price: 1000)
-        visit "/merchants/#{merchant.id}/invoices/#{invoice_1.id}"
-
-        expect(page).to_not have_content("222")
-        expect(page).to_not have_content("3499")
-        expect(page).to_not have_content("Polearm")
+    expect(page).to_not have_content("222")
+    expect(page).to_not have_content("3499")
+    expect(page).to_not have_content("Polearm")
   end
-   it 'shows total revenue', :vcr do
-        merchant = Merchant.create(name: "Braum's")
-        merchant2 = Merchant.create(name: "Target")
 
-        item1 = merchant.items.create(name: "Toast", description: "Let it rip!", unit_price: 1000)
-        item2 = merchant.items.create(name: "Polearm", description: "Let it rip!", unit_price: 1000)
+ it 'shows total revenue', :vcr do
+    merchant = Merchant.create(name: "Braum's")
+    merchant2 = Merchant.create(name: "Target")
 
-        bob = Customer.create!(first_name: "Bob", last_name: "Benson")
+    item1 = merchant.items.create(name: "Toast", description: "Let it rip!", unit_price: 1000)
+    item2 = merchant.items.create(name: "Polearm", description: "Let it rip!", unit_price: 1000)
 
-        invoice_1 = bob.invoices.create!(status: 1, created_at: '05 Apr 2022 00:53:36 UTC +00:00')
-        invoice_2 = bob.invoices.create!(status: 1, created_at: '05 Apr 2022 00:53:36 UTC +00:00')
+    bob = Customer.create!(first_name: "Bob", last_name: "Benson")
 
-        invoice_item_1 = item1.invoice_items.create(invoice_id:invoice_1.id, quantity:45, unit_price: 1000)
-        invoice_item_2 = item2.invoice_items.create(invoice_id:invoice_1.id, quantity:222, unit_price: 1000)
-        visit "/merchants/#{merchant.id}/invoices/#{invoice_1.id}"
+    invoice_1 = bob.invoices.create!(status: 1, created_at: '05 Apr 2022 00:53:36 UTC +00:00')
+    invoice_2 = bob.invoices.create!(status: 1, created_at: '05 Apr 2022 00:53:36 UTC +00:00')
 
-        expect(page).to have_content("2670.00")
+    invoice_item_1 = item1.invoice_items.create(invoice_id:invoice_1.id, quantity:45, unit_price: 1000)
+    invoice_item_2 = item2.invoice_items.create(invoice_id:invoice_1.id, quantity:222, unit_price: 1000)
+    visit "/merchants/#{merchant.id}/invoices/#{invoice_1.id}"
 
+    expect(page).to have_content("2670.00")
   end
+
+  it 'displays the total revenue after bulk discounts are applied' do
+    merchant = Merchant.create(name: "Braum's")
+    item1 = merchant.items.create(name: "Toaster", description: "Four slots", unit_price: 1000)
+    item2 = merchant.items.create(name: "Poleaxe", description: "8 foot reach!", unit_price: 1000)
+
+    bob = Customer.create!(first_name: "Bob", last_name: "Benson")
+
+    invoice_1 = bob.invoices.create!(status: 1, created_at: '05 Apr 2022 00:53:36 UTC +00:00')
+    invoice_2 = bob.invoices.create!(status: 1, created_at: '05 Apr 2022 00:53:36 UTC +00:00')
+
+    invoice_item_1 = item1.invoice_items.create(invoice_id:invoice_1.id, quantity:45, unit_price: 1000)
+    invoice_item_2 = item2.invoice_items.create(invoice_id:invoice_1.id, quantity:222, unit_price: 1000)
+
+    merchant.bulk_discounts.create!(name: 'Mega Liquidation', discount_percentage: 50, quantity_threshold: 100)
+    merchant.bulk_discounts.create!(name: 'Yeehaw Sale', discount_percentage: 30, quantity_threshold: 40)
+
+    visit "/merchants/#{merchant.id}/invoices/#{invoice_1.id}"
+
+    expect(page).to have_content("2670.00")
+    expect(page).to have_content("1425.00")    
+  end
+
 
   describe 'as a merchant' do
     describe 'when i visit my merchant invoice show page' do
@@ -95,7 +114,7 @@ RSpec.describe 'the merchant invoice show page' do
       end
 
       it "i see that each invoice item status is a select field
-          and i see that the invoice item's current status is selected", :vcr do
+        and i see that the invoice item's current status is selected", :vcr do
 
         within "#item-#{@invoice_item_1.id}" do
           expect(page).to have_select(:status, selected: 'Pending')
