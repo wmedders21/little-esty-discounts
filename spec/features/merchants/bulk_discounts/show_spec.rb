088feb4d25d8d2a593766aa63d22bbf0f6647132
Will Medders <wmedders@gmail.com>
2022-04-24 16:48:00 -0400

    FEATUREtest: BD show can edit BD

--- spec/features/merchants/bulk_discounts/show_spec.rb
+++ spec/features/merchants/bulk_discounts/show_spec.rb
@@ -14,4 +14,25 @@ RSpec.describe 'bulk discounts show' do
     expect(page).to have_no_content("10")
     expect(page).to have_no_content("100")
   end
+
+  it 'can edit the discount' do
+    merchant_1 = Merchant.create(name: "Braum's")
+    big_deal = merchant_1.bulk_discounts.create!(name: 'Big Deal', discount_percentage: 25, quantity_threshold: 5)
+    visit "/merchants/#{merchant_1.id}/bulk_discounts/#{big_deal.id}"
+
+    expect(page).to have_content("25")
+    expect(page).to have_content("5")
+
+    click_link "Edit Discount"
+    fill_in :name, with: "Bigger Deal"
+    fill_in :discount_percentage, with: "30"
+    fill_in :quantity_threshold, with: "3"
+    click_button "Submit"
+
+    expect(current_path).to eq("/merchants/#{merchant_1.id}/bulk_discounts/#{big_deal.id}")
+    expect(page).to have_content("30")
+    expect(page).to have_content("3")
+    expect(page).to have_no_content("25")
+    expect(page).to have_no_content("5")
+  end    
 end
